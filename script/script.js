5e4418bd05035a5455522d868c5b2a6bba33c1f5
Daniel Oliveira <danielcoliv98@gmail.com>
2022-02-03 16:09:49 -0300

    Criando funçao que vira a carta

--- script/script.js
+++ script/script.js
@@ -1,11 +1,19 @@
 
 
-let numberOfCards = askCards();
+// let numberOfCards = askCards();
 
-function askCards () {
-do{
-    number = prompt("Quantas cartas você deseja? (números pares de 4 à 14)")
-} while(number   < 4 || number > 16 || number %2 != 0)
-console.log(number  );
-return number;
+// function askCards () {
+// do{
+//     number = prompt("Quantas cartas você deseja? (números pares de 4 à 14)")
+// } while(number   < 4 || number > 16 || number %2 != 0)
+// console.log(number  );
+// return number;
+// }
+
+
+function flip (element) {
+
+  element.querySelector(".front").classList.toggle("flip")
+  element.querySelector(".back").classList.toggle("flip2")
 }
+
