c4f9e47e2056b84056bd3be67b4c3ca44cd8fe86
Daniel Oliveira <danielcoliv98@gmail.com>
2022-02-04 14:45:37 -0300

    Funçao cria as cartas de acordo com o prompt

--- script/script.js
+++ script/script.js
@@ -1,39 +1,60 @@
+const numberOfCards = askCards() / 2;
+
+function askCards() {
+  do {
+    number = prompt("Quantas cartas você deseja? (números pares de 4 à 14)")
+  } while (number < 4 || number > 14 || number % 2 != 0)
+  return number;
+}
+const cards = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+const cardsInGame = [];
+
+cards.sort(comparador);
+console
+function createCardsInGame() {
+
+  for (let i = 0; i < numberOfCards; i++) {
+    cardsInGame.push(cards[i]);
+    cardsInGame.push(cards[i]);
+  }
+  cardsInGame.sort(comparador);
+  console.log(cardsInGame)
+}
+ 
+function createCards() {
+  let card = document.querySelector("section");
+
+  for (let i = 0; i < cardsInGame.length; i++) {
+    card.innerHTML += `
+  <div onclick="flip(this)" class="card ${cards[i]}">
+  <div  class="front face">
+      <img src="assets/front.png" alt="">
+  </div>
+  <div  class="back face">
+      <img src="assets/${cards[i]}.gif" alt="">
+  </div>
+</div>`}
+
+}
+
+function cardsDisplay() {
+
+  createCardsInGame();
+  createCards();
+
+}
+cardsDisplay()
 
 
-// let numberOfCards = askCards();
 
-// function askCards () {
-// do{
-//     number = prompt("Quantas cartas você deseja? (números pares de 4 à 14)")
-// } while(number   < 4 || number > 16 || number %2 != 0)
-// console.log(number  );
-// return number;
-// }
-const cards = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
-
-
-
-
-
-
-
-function flip (element) {
+function flip(element) {
 
   element.querySelector(".front").classList.toggle("flip")
   element.querySelector(".back").classList.toggle("flip2")
 }
 
-function createCards(index){
-  let card = document.querySelector("section");
-  card.innerHTML += `
-  <div onclick="flip(this)" class="card">
-  <div  class="front face">
-      <img src="assets/front.png" alt="">
-  </div>
-  <div  class="back face">
-      <img src="assets/${cards[index]}" alt="">
-  </div>
-</div>`
+function comparador() {
+  return Math.random() - 0.5;
 }
 
-createCard();
+
